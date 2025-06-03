d8ac34b52632e9a0b4233c298d36397bf51d3eb4
mahmoudmatter12 <HNU.24.363.Mahmoud-zEl-Din@hnu.edu.eg>
2025-06-03 21:27:37 +0300

    meta data

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -4,7 +4,11 @@ import { StatisticsSection } from "@/components/home/components/StatisticsSectio
 import { HowItWorksSection } from "@/components/home/components/HowItWorksSection";
 import { CTASection } from "@/components/home/components/CTASection";
 import SponsorsSection from "@/components/home/components/SponsorsSection";
-
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Home | Campus Core",
+  description: "Welcome to the Campus Core homepage. Discover features, statistics, and more.",
+};
 export default async function HomePage() {
   return (
     <>
